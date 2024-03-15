d29431faef48843e3b8cac0e789f09ab2f1a97e6
Klande <jhonasemece@gmail.com>
2024-03-15 03:44:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# JM Portafolio Web
+Este es mi portafolio web, esta hecho con **HTML, CSS y JavaScript** 
+# Frame Works
+Los **Frame Works** que use en este proyecto son: **Bootstrap, Google Fonts, Scrollreveal**
+# Cosas por fixear
+- Los iconos del NavBar.
---
+++ js/main.js
@@ -0,0 +1,38 @@
+// ScrollReveal
+
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar-container', {
+    duration: 1500,
+    origin: 'top',
+    distance: '100px',
+    reset: true
+});
+
+sr.reveal('.home-cont', {
+    duration: 1500,
+    origin: 'left',
+    distance: '100px',
+    reset: true
+});
+
+sr.reveal('.lang-icons', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '100px',
+    reset: true
+});
+
+sr.reveal('.tools-icons', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '100px',
+    reset: true
+});
+
+sr.reveal('.proyects', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '100px',
+    reset: true
+});
