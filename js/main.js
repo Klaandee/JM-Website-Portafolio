e7aa44e155b8c308158616617262150d1e83bc32
Klande <jhonasemece@gmail.com>
2024-03-16 02:32:46 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ $form.addEventListener('submit', handleSubmit);
 function handleSubmit(event){
     event.preventDefault();
     const form = new FormData(this);
-    $buttonMailTo.setAttribute('href', `mailto:jh0n4as@gmail.com?subject=Asunt: ${form.get('asunt')} Email: ${form.get('email')}&body=${form.get('message')}`);
+    $buttonMailTo.setAttribute('href', `mailto:jh0n4as@gmail.com?subject=Asunt: ${form.get('asunt')}&body=${form.get('message')}`);
     $reloadWeb.setAttribute('href', `index`);
     $buttonMailTo.click();
     $reloadWeb.click();
