73a4bcaf785f06a12ad9be73f457cfa2bb09642d
Klande <jhonasemece@gmail.com>
2024-03-21 22:36:12 -0300

    Changelogs Page Finished

---
+++ js/changelogs.js
@@ -0,0 +1,17 @@
+// ScrollReveal
+
+window.sr = ScrollReveal();
+
+sr.reveal('.changelogs', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '100px',
+    reset: true
+});
+
+sr.reveal('.change-title', {
+    duration: 1500,
+    origin: 'top',
+    distance: '100px',
+    reset: true
+});
